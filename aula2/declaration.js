18da4660cf43f4a38700d037e4d046268ca09b99
Rodrigo <2148722@student.uma.pt>
2026-03-03 00:05:02 +0000

    declaration

---
+++ aula2/declaration.js
@@ -0,0 +1,27 @@
+function factorial(number) {
+    var fact=1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+        
+    }
+    return fact;
+}
+
+function main() {
+
+    var notaP1 = 15;
+    var notaP2 = 20;
+    var notaPF = 10;
+
+    var notaF= notaFinal(notaP1, notaP2, notaPF);
+    console.log("A nota final é: " + notaF);
+
+    var f=factorial(5);
+    console.log("O fatorial de 5 é: " + f);
+
+    var numbers = [1,1,1,1,1,1,1,1,1,1];
+    var avg= average(numbers);
+
+    console.log("A média é: " + avg);
+}
+main();
