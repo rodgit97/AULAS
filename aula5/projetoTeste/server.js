22c72550a09e0d634dfdbd7735c2643f0dcc456e
Rodrigo <2148722@student.uma.pt>
2026-03-17 16:48:09 +0000

    aula5aula6

---
+++ aula5/projetoTeste/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Exemplo app listening http://%s:%s", host, port);
+});
+app.get("/", function (req, res) {
+  response.send("boas");
+});
+
+app.post("/users", function (req, res) {
+  var id = req.body.id;
+  response.send("Post user");
+});
+
+app.delete("/users/:id", function (req, res) {
+  var id = req.params.id;
+  response.send("Delete user");
+});
+
+// var fs = require("fs");
+// var html= fs.readFileSync("./index.html",'utf-8');
